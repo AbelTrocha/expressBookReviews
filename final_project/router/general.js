f3f92b89ffd8c93a6f689c04594aaab081f918fc
Abel Trocha <abel.trocha@gmail.com>
2025-05-25 19:15:21 -0400

    tarea 5 get review/:isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -56,13 +56,17 @@ public_users.get('/title/:title',function (req, res) {
         }
     }
   );
-  return res.status(300).json({message: "Titulo no encontrado"});
+  return res.status(300).json({message: "Titulo no encontrado."});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if(books[isbn]){
+    return res.send(JSON.stringify(books[isbn].reviews));
+  }else{
+    return res.status(300).json({message: "Libro no encontrado."});
+  }
 });
 
 module.exports.general = public_users;
